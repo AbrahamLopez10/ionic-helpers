50d12061e0b9e9fea913705151bbb93516b46bd1
Abraham Lopez <ab@aplimovil.mx>
2017-07-20 03:51:00 -0500

    improvements to AbstractQueue

--- providers/abstract-queue.ts
+++ providers/abstract-queue.ts
@@ -1,71 +1,131 @@
 import { Util } from './../libs/util';
-import { UI } from './../libs/ui';
-import { Translator } from './translator';
-import { AlertController } from 'ionic-angular';
-import { Translate } from './translate';
 import { AbstractAPIService } from "./abstract-api-service";
+import { sprintf } from "../libs/sprintf";
 
-export class AbstractQueue<T> {
+export abstract class AbstractQueue<T> {
   protected api: AbstractAPIService;
-  protected alertCtrl: AlertController;
-  protected tr: Translate;
-  protected queue: T[] = [];
+  protected items: T[] = [];
   protected T_Class: any;
+  private storageKey: string;
+  private timer: number;
 
-  protected STORAGE_KEY = 'queue';
+  protected INTERVAL = 5000;
 
-  constructor(translator: Translator, T_Class?: any) {
-    this.tr = new Translate(translator);
+  constructor(T_Class?: any, storageKey?: string) {
+    this.storageKey = storageKey ? storageKey : this.constructor.name;
     if(T_Class) this.T_Class = T_Class;
+    this.log('Instantiated');
+  }
+
+  abstract process(): void;
+
+  start() {
+    this.log('Started');
+
+    this.timer = setInterval(() => {
+      if(this.items.length != 0){
+        this.log('Processing...'); 
+        this.process();
+      } else {
+        this.log('Empty queue, waiting.');
+      }
+    }, this.INTERVAL);
+  }
+
+  stop() {
+    this.log('Stopped');
+    clearInterval(this.timer);
+  }
+
+  init() {
+      this.load();
+      this.start();
+  }
+
+  log(...params) {
+      if(typeof params[0] == 'string'){
+          params[0] = `[${this.constructor.name}] ` + params[0];
+      }
+
+      console.log.apply(this, params);
   }
 
   async load() {
-    try{
-        let queueJson = await this.api.getCacheItem(this.STORAGE_KEY);
-        
-        if(queueJson){
-            let queue: T[] = Util.parseJSON(queueJson);
+    this.log('Loading');
 
-            if(queue instanceof Array){
-                if(this.T_Class){
-                    let T_Class = this.T_Class;
-                    this.queue = queue.map(item => new T_Class(item));
-                } else {
-                    this.queue = queue;
-                }
-            }
-        }
+    try{
+      let queueJson = await this.api.getCacheItem(this.storageKey);
+      
+      if(queueJson){
+          let queue: T[] = Util.parseJSON(queueJson);
+
+          if(queue instanceof Array){
+              if(this.T_Class){
+                  let T_Class = this.T_Class;
+                  this.items = queue.map(item => new T_Class(item));
+              } else {
+                  this.items = queue;
+              }
+
+              this.log('Loaded: ', queue);
+          } else {
+            this.log('Retrieved queue data is invalid: ', queueJson); 
+          }
+      } else {
+        this.log('Retrieved queue is empty.'); 
+      }
     } catch(error){
-      UI.alert(this.alertCtrl, this.tr._('Queue load error: ', error));
+      console.error(sprintf('%s load error: ', this.constructor.name, error));
     }
   }
 
   async save() {
-    let queueJson = JSON.stringify(this.queue);
-    await this.api.setCacheItem(this.STORAGE_KEY, queueJson);
+    let queueJson = JSON.stringify(this.items);
+    await this.api.setCacheItem(this.storageKey, queueJson);
   }
 
-  getItems() {
-      return this.queue.slice();
+  find(key, value) {
+    for(let item of this.items){
+      if(item[key] === value){
+        return item;
+      }
+    }
+
+    return null;
+  }
+
+  getItems(byRef: boolean = false) {
+      return byRef ? this.items : this.items.slice();
   }
 
   getCount() {
-    return this.queue.length;
+    return this.items.length;
   }
 
   push(item: T) {
-    this.queue.push(item);
+    this.items.push(item);
+    this.log('Pushed item: ', item);
     this.save();
   }
 
   peek(): T {
-      return this.queue.length != 0 ? this.queue[0] : null;
+      return this.items.length != 0 ? this.items[0] : null;
   }
 
   pop(): T {
-      if(this.queue.length == 0) return null;
-      let item = this.queue.shift();
+      if(this.items.length == 0) return null;
+      let item = this.items.shift();
+      this.log('Popped item: ', item);
       this.save();
       return item;
+  }
+
+  popToBack(): T {
+    if(this.items.length == 0) return null;
+    let item = this.items.shift();
+    this.items.push(item);
+    this.log('Popped item to back: ', item);
+    this.save();
+    return item;
   }
 }
